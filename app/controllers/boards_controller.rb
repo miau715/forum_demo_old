7e5e743302a4a2501607b34a10fb044699c03429
Miau Huang <miau715@gmail.com>
2011-10-03 02:01:40 +0800

    add CRUD action in post and board controller

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,2 +1,33 @@
 class BoardsController < ApplicationController
+  def index
+    @boards = Board.all
+  end
+  
+  def new
+    @board = Board.new
+  end
+  
+  def create
+    @board = Board.new(params[:board])
+    @board.save
+  end
+  
+  def show
+    @board = Board.find(params[:id])
+  end
+  
+  def edit
+    @board = Board.find(params[:id])
+  end
+  
+  def update
+    @board = Board.find(params[:id])
+    @board.update_attributes(params[:board])
+  end
+  
+  def destroy
+    @board = Board.find(params[:id])
+    @board.destroy
+  end
+  
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,33 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+  
+  def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new(params[:post])
+    @post.save
+  end
+  
+  def show
+    @post = Post.find(params[:id])
+  end
+  
+  def edit
+    @post = Post.find(params[:id])
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+    @post.update_attributes(params[:post])
+  end
+  
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+  end
+  
 end
