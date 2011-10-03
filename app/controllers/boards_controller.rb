010640f0bfc4c5432ae26dfd68fa171a4661710e
Miau Huang <miau715@gmail.com>
2011-10-03 10:22:13 +0800

    complete boards and posts controller

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,33 +1,73 @@
 class BoardsController < ApplicationController
   def index
     @boards = Board.all
+    
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @boards }
+    end
   end
   
   def show
     @board = Board.find(params[:id])
+    
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @board }
+    end
   end
   
   def new
     @board = Board.new
+    
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @board }
+    end
+  end
+  
+  def edit
+    @board = Board.find(params[:id])
   end
   
   def create
     @board = Board.new(params[:board])
     @board.save
-  end
-
-  def edit
-    @board = Board.find(params[:id])
+    
+    respond_to do |format|
+      if @board.save
+        format.html { redirect_to(@board, :notice => 'Board was successfully created.') }
+        format.xml  { render :xml => @board, :status => :created, :location => @board }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @board.errors, :status => :unprocessable_entity }
+      end
+    end
   end
   
   def update
     @board = Board.find(params[:id])
     @board.update_attributes(params[:board])
+    
+    respond_to do |format|
+      if @board.update_attributes(params[:board])
+        format.html { redirect_to(@board, :notice => 'Board was successfully updated.') }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @board.errors, :status => :unprocessable_entity }
+      end
+    end
   end
   
   def destroy
     @board = Board.find(params[:id])
     @board.destroy
+    
+    respond_to do |format|
+      format.html { redirect_to(boards_url) }
+      format.xml  { head :ok }
+    end
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ ForumDemo::Application.routes.draw do
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :posts
+  resources :boards
 
   # Sample resource route with options:
   #   resources :products do
